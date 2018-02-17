f1ac20443b07726ed331f751ec4072a1975b2263
Ben Frankel <ben.frankel7@gmail.com>
2018-02-17 13:57:48 -0600

    Quit REPL with EOF

--- repl.py
+++ repl.py
@@ -2,18 +2,20 @@ import generate
 
 generate.load()
 
-print('Enter a period at any time to end the session')
-print('Enter a comma to reload categories and templates')
-print()
+print(('Enter a period at any time to end the session\n'
+       'Enter a comma to reload categories and templates\n'))
 
 while True:
-    acronym = input('Backronym of: ')
+    try:
+        acronym = input('Backronym of: ')
+    except EOFError:
+        break
     if acronym == '.':
         break
-    elif acronym == ',':
+
+    if acronym == ',':
         generate.load()
         print('Reloaded successfully')
-        print()
     else:
         print(acronym.upper(), '=', generate.all_backronyms(acronym))
-        print()
+    print()
